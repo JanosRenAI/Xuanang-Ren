f952a24a450d9f86bce47faa5867031e35b18b0e
GenosAI <janosren520@gmail.com>
2024-09-12 22:42:04 -0400

    change in dofizzbuzz

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -10,62 +10,39 @@ class FizzBuzz {
 
         while (i < 100) {
 
-            // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
-
-            // Print the appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
-
-                System.out.println("Fizz Buzz");
-
-            } else if (divisibleBy3) {
-
-                System.out.println("Fizz");
-
-            } else if (divisibleBy5) {
-
-                System.out.println("Buzz");
-
-            } else {
-
-                System.out.println(i);
-
-            }
-
-            i++; // Increment the counter
+            i = doFizzBuzz(i);
         }
     }
 
-    public static void doFizzBuzz(int j) {
-        int i = 1;
-        while (i < j) {
+    public static int doFizzBuzz(int i) {
+        // Find out which numbers divide i.
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
 
-            // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
+        // Print the appropriate result.
+        if (divisibleBy3 && divisibleBy5) {
 
-            // Print the appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
+            System.out.println("Fizz Buzz");
 
-                System.out.println("Fizz Buzz");
+        } else if (divisibleBy3) {
 
-            } else if (divisibleBy3) {
+            System.out.println("Fizz");
 
-                System.out.println("Fizz");
+        } else if (divisibleBy5) {
 
-            } else if (divisibleBy5) {
+            System.out.println("Buzz");
 
-                System.out.println("Buzz");
+        } else {
 
-            } else {
+            System.out.println(i);
 
-                System.out.println(i);
-
-            }
-            i++; // Increment the counter
         }
 
-
+        i++; // Increment the counter
+        return i;
     }
+
+    //public static void doFizzBuzz(int j) {
+        
+    //}
 }
